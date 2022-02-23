24cfdab7b0d3962e0f7a378f4760056a27330ef2
linong <linong@kuaishou.com>
2022-02-23 09:39:32 +0800

    feat: 新增摇树优化例子

---
+++ packages/vue-import-env-shaking/src/main.js
@@ -0,0 +1,29 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+if (false) {
+    console.log('lilnong-1', `false`)
+    const component = await (async() => await
+        import ('./components/a.vue'));
+    console.log('lilnong-1', `false`, component)
+}
+
+if (process.env.NODE_ENV === 'development') {
+    console.log('lilnong-1', `process.env.NODE_ENV === 'development'`)
+    const component = await (async() => await
+        import ('./components/b.vue'));
+    console.log('lilnong-1', `process.env.NODE_ENV === 'development'`, component)
+}
+
+if (localStorage.debug) {
+    console.log('lilnong-1', `localStorage.debug`)
+    const component = await (async() => await
+        import ('./components/c.vue'));
+    console.log('lilnong-1', `localStorage.debug`, component)
+}
